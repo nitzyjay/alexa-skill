f73756342e9b067f42107ba0bba1dc3766491c6c
nitzyjay <nitzyjay@gmail.com>
2021-03-14 13:13:06 +0100

    Adding Get method

--- Controllers/AlexaController.cs
+++ Controllers/AlexaController.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace Eureka.Controllers
 {
+    //[ApiController]
+    //[Route("[controller]")]
+    //public class AlexaController : ControllerBase
     public class AlexaController
-    {
-        //public AlexaController()
-        //{
-        //}
+    {            
         [HttpPost, Route("api/alexa/demo")]
         public dynamic Nitish(dynamic request)
         {
@@ -31,6 +35,13 @@ namespace Eureka.Controllers
                     shouldEndSession = true
                 }
             };
+        }
+
+        [HttpGet, Route("api/alexa/demo")]
+        public dynamic Get()
+        {
+            var rng = new Random();
+            return "The Web API is up and running";
         }
     }
 }
