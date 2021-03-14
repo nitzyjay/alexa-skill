34755c004cf02fd3ead2192e1d06712c8d61e2da
nitzyjay <nitzyjay@gmail.com>
2021-03-14 12:55:44 +0100

    Initial check-in of module Eureka

---
+++ Controllers/AlexaController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eureka.Controllers
+{
+    public class AlexaController
+    {
+        //public AlexaController()
+        //{
+        //}
+        [HttpPost, Route("api/alexa/demo")]
+        public dynamic Nitish(dynamic request)
+        {
+            return new
+            {
+                version = "1.0",
+                sessionAttributes = new { },
+                response = new
+                {
+                    outputSpeech = new
+                    {
+                        type = "PlainText",
+                        text = "Hello from Nitish"
+                    },
+                    card = new
+                    {
+                        type = "Simple",
+                        title = "Nitish",
+                        content = "Hello from Nitish"
+                    },
+                    shouldEndSession = true
+                }
+            };
+        }
+    }
+}
